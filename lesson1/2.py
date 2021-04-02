e19be615acc314865bce2bc3f2d5c7efebd1dd19
Tiashe <tiashe@live.ru>
2021-04-02 19:20:22 +0300

    готово

--- lesson1/2.py
+++ lesson1/2.py
@@ -1,18 +1,31 @@
-timesec = int(input('Введите время в секундах - '))
-timeminute = int(0)
-timehour = int(0)
+n = int(input('ВВЕДИТЕ ВРЕМЯ В СЕКУНДАХ: '))
 
-while timesec > 60:
-    timeminute = timeminute + (timesec / 60)
-    continue
-while timeminute > 60:
-    timehour = timehour + (timeminute / 60)
-    continue
-#if timesec < 60:
-    print('Время - 00:00:', timesec)#
+if n > 86400:
+    print('ЗАДАННОЕ ВРЕМЯ В СЕКУНДАХ БОЛЬШЕ 1 ДНЯ')
 
-print('Время - ', timehour %d, ':', timeminute %d, ':', timesec %d)
+hours = (n // 3600) % 24
+# подсчитываем часы
 
+minutes = (n // 60) % 60
+# подсчитываем минуты
 
+seconds = n % 60
+# подсчитываем остаток секунд
 
+if hours < 10:
+    # если время в часах меньше 10 добавляем ноль впереди
+    hours = str('0' + str(hours))
+else:
+    hours = str(hours)
+if minutes < 10:
+    # если время в минутах меньше 10 добавляем ноль впереди
+    minutes = str('0' + str(minutes))
+else:
+    minutes = str(minutes)
+if seconds < 10:
+    # если остаток в секундах меньше 10 добавляем ноль впереди
+    seconds = str('0' + str(seconds))
+else:
+    seconds = str(seconds)
 
+print(str(hours)+':'+str(minutes)+':'+str(seconds))
